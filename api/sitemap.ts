67aa1f5ac76efca8d75448e4930138f12f5b0b30
User Name <user@example.com>
2026-07-01 13:09:26 +0530

    SEO/AEO/GEO: Fix duplicate and incorrect xmlns namespace declarations in sitemap XML opening tag

--- api/sitemap.ts
+++ api/sitemap.ts
@@ -95,10 +95,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     .join('');
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-  xmlns="http://www.w3.org/2001/XMLSchema-instance"
-  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 ${staticUrls}
 ${blogUrls}
 </urlset>`;
